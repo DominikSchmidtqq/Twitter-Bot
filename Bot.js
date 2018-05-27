d2f60c69c8145a4a95899b636208ed6e17a0812d
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2018-05-26 20:53:49 -0700

    reduced redundancy
    /*

--- Bot.js
+++ Bot.js
@@ -1,15 +1,30 @@
 console.log('Bot is starting');
 
 var Twit = require('twit');
-
 var APIKeys = require('./APIKeys');
-
 var T = new Twit(APIKeys);
 
-var arguments = { q: 'twitter', count: 1 };
-
-T.get('search/tweets', arguments, gotData);
+function getTweet(text, numberOfTweets) {
+    var arguments = { q: text, count: numberOfTweets };
+    T.get('search/tweets', arguments, gotData);
+}
 
 function gotData(err, data, response) {
     console.log(data);
-};
+}
+
+function postTweet(text) {
+    var tweetText = {
+        status: text
+    };
+
+    T.post('statuses/update', tweetText, tweetCallback);
+}
+
+function tweetCallback(err, data, response) {
+    if (!err) {
+        console.log("Success");
+    }
+}
+
+postTweet("Test");
